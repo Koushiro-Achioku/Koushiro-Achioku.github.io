26a55841db357b3ec486686679a9fd1cc652d919
Koushiro-Achioku <achioku.koushiro@gmail.com>
2021-01-09 01:12:57 +0900

    Commit 2021 1/9 1:13 by Koushiro Achioku

---
+++ TEST_Ranking/Code.js
@@ -0,0 +1,20 @@
+function doGet() {
+    var htmlOutput = HtmlService.createTemplateFromFile("index").evaluate();
+    htmlOutput.addMetaTag('viewport','width=device-width, initial-scale=1, user-scalable=no');
+    return htmlOutput;
+}
+
+function getSheet(name){
+    // SSIDからスプレッドシートの取得
+    var ssId = '1KiP4221dHW_Vv5erhFDPICwEpu2jwErTXx9oAYyC1GQ';
+    var ss = SpreadsheetApp.openById(ssId);
+    // 指定されたシート名からシートを取得して返却
+    var sheet = ss.getSheetByName(name);
+    return sheet;
+}
+
+function getData(name) {
+    // 指定したシートからデータを取得
+    var values = getSheet(name).getDataRange().getValues();
+    return values;
+}

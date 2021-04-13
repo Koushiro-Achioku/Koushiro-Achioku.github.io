14befd81c024c79ff41b0c727ed69620b9cc5981
Koushiro <achioku.koushiro@gmail.com>
2021-04-13 20:05:26 +0900

    committed on 2021/4/13 20:05 by Koushiro

---
+++ JuniorEx/Ranking/Code.js
@@ -0,0 +1,22 @@
+function doGet() {
+    var htmlOutput = HtmlService.createTemplateFromFile("index").evaluate();
+    htmlOutput.addMetaTag('viewport','width=device-width, initial-scale=1, user-scalable=no');
+    htmlOutput.setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+    return htmlOutput;
+}
+
+function getSheet(name){
+    // SSIDからスプレッドシートの取得
+    //https://docs.google.com/spreadsheets/d/hoge
+    var ssId = 'hoge';
+    var ss = SpreadsheetApp.openById(ssId);
+    // 指定されたシート名からシートを取得して返却
+    var sheet = ss.getSheetByName(name);
+    return sheet;
+}
+
+function getData(name) {
+    // 指定したシートからデータを取得
+    var values = getSheet(name).getDataRange().getValues();
+    return values;
+}

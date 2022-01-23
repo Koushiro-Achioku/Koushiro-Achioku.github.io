d6026b0efd5dd2ca91f463d53fae689aa1a531bd
Koushiro Achioku <achioku.koushiro@gmail.com>
2022-01-24 01:52:46 +0900

    commit by Koushiro Achioku at 2022/1/24 1:53

--- 2021/SeniorEx/Main/Code.js
+++ 2021/SeniorEx/Main/Code.js
@@ -1,8 +1,8 @@
 var passcode = '123456';
 
 function doGet() {
-  　//エラー表示
-  　var htmlOutput = HtmlService.createTemplateFromFile("error").evaluate();
+   //エラー表示
+    var htmlOutput = HtmlService.createTemplateFromFile("error").evaluate();
     htmlOutput.setTitle('エラー');
     htmlOutput.addMetaTag('viewport','width=device-width, initial-scale=1, user-scalable=no');
     return htmlOutput;
@@ -11,16 +11,18 @@ function doGet() {
 function doPost(e) {
   if(e.parameter.Passcode == passcode){
     // シートを取得
-    var sheet = getSheet('Base Data');
+    var sheet = getSheet('Base');
 
     // シートの最終行を取得
-    var lastRow = sheet.getLastRow();
+    //var lastRow = sheet.getLastRow();
 
     // 最終行にデータ挿入
     // 「e.parameter.フォーム名」 でフォームから送信されたパラメータを受け取ることができます
-    sheet.appendRow([new Date(),e.parameter.TeamID,e.parameter.Round,e.parameter.Point,e.parameter.Time,e.parameter.Retire,
-        e.parameter.Mission1_1,e.parameter.Mission1_2,e.parameter.Mission1_3,e.parameter.Mission2,e.parameter.Mission3,
-        e.parameter.Mission4,e.parameter.Mission5_1,e.parameter.Mission5_2]);
+    sheet.appendRow([new Date(),e.parameter.TeamID,e.parameter.Round,e.parameter.Point,e.parameter.Time,
+                    e.parameter.Mission1_1,e.parameter.Mission1_2,e.parameter.Mission1_3,
+                    e.parameter.Mission2_1,e.parameter.Mission2_2,e.parameter.Mission2_3,e.parameter.Mission2_4,
+                    e.parameter.Mission3_1,e.parameter.Mission4_1,
+                    e.parameter.Mission5_1,e.parameter.Mission5_2,e.parameter.Mission5_3]);
 
     calculate();
     sort();
@@ -40,125 +42,208 @@ function doPost(e) {
 
 function getSheet(name){
     // SSIDからスプレッドシートの取得
-    //https://docs.google.com/spreadsheets/d/1ura54uUdaOLdgg9DY-cHIapp-wN-nLY69U0s7z-7Qjk/edit?usp=sharing
-    var ssId = '1ura54uUdaOLdgg9DY-cHIapp-wN-nLY69U0s7z-7Qjk';
-    var ss = SpreadsheetApp.openById(ssId);
+    //https://docs.google.com/spreadsheets/d/1yx_MdZquMbDgQTdLdCzGDAl9PTQAOrstxAbOxj0DDb4/edit#gid=0
+    var ssid = '1yx_MdZquMbDgQTdLdCzGDAl9PTQAOrstxAbOxj0DDb4';
+    var ss = SpreadsheetApp.openById(ssid);
     // 指定されたシート名からシートを取得して返却
     var sheet = ss.getSheetByName(name);
     return sheet;
 }
 
-function getData(name) {
-    // 指定したシートからデータを取得
-    var values = getSheet(name).getDataRange().getValues();
-    return values;
-}
-
 function calculate(){
-  var sheet = getSheet('Base Data');//Sheetの指定
+  var sheet = getSheet('Base');//Sheetの指定
   const columnBVals = sheet.getRange('A:A').getValues(); // A列値を配列で取得
   const LastRow = columnBVals.filter(String).length;  //空白を除き、配列の数を取得
   
-  var sheet2 = getSheet('Total Data');//Sheetの指定
+  var sheet2 = getSheet('Total');//Sheetの指定
   const columnBVals2 = sheet2.getRange('A:A').getValues(); // A列値を配列で取得
   const LastRow2 = columnBVals2.filter(String).length;  //空白を除き、配列の数を取得
   
-  for(var i=2; i<LastRow+1; i++){
-    //BaseDataからcheckしていない行を探索
-    const check_column = 15;
+  for(var i=2; i<=LastRow; i++){
+    //BaseDataからcheckがYesでない行を探索
+    const check_column = 18;
     if(sheet.getRange(i,check_column).getValue()!="Yes"){
       
       var id_position=0;
-      for(var ii=2; ii<LastRow2+2; ii++){
+      for(var ii=3; ii<=LastRow2+1; ii++){
         if(sheet.getRange(i,2).getValue()==sheet2.getRange(ii,1).getValue()){
           id_position=ii;
           break;
         }
       }
-      Logger.log(id_position);
+      //Logger.log(id_position);
       
       var Point = sheet.getRange(i,4).getValue();
       var Time = sheet.getRange(i,5).getValue();
-      var Retire = sheet.getRange(i,6).getValue();
       
       //Score書き込み
       if(sheet.getRange(i,3).getValue()=="1"){
-        sheet2.getRange(id_position,6).setValue(Point);//Point
-        sheet2.getRange(id_position,7).setValue(Time);//Time
-        sheet2.getRange(id_position,8).setValue(Retire);//Retire
+        sheet2.getRange(id_position,12).setValue(Point);//Point
+        sheet2.getRange(id_position,13).setValue(Time);//Time
+        sheet2.getRange(id_position,14).setValue(i);//Row
+
+        sheet2.getRange(id_position,3).setValue(1);//Round
+        sheet2.getRange(id_position,4).setValue(Point);//Point
+        sheet2.getRange(id_position,5).setValue(Time);//Time
       }
       else if(sheet.getRange(i,3).getValue()=="2"){
-        sheet2.getRange(id_position,9).setValue(Point);//Point
-        sheet2.getRange(id_position,10).setValue(Time);//Time
-        sheet2.getRange(id_position,11).setValue(Retire);//Retire
+        sheet2.getRange(id_position,15).setValue(Point);//Point
+        sheet2.getRange(id_position,16).setValue(Time);//Time
+        sheet2.getRange(id_position,17).setValue(i);//Row
+
+        sheet2.getRange(id_position,6).setValue(2);//Round
+        sheet2.getRange(id_position,7).setValue(Point);//Point
+        sheet2.getRange(id_position,8).setValue(Time);//Time
       }
-      
+      else if(sheet.getRange(i,3).getValue()=="3"){
+        sheet2.getRange(id_position,18).setValue(Point);//Point
+        sheet2.getRange(id_position,19).setValue(Time);//Time
+        sheet2.getRange(id_position,20).setValue(i);//Row
+
+        sheet2.getRange(id_position,9).setValue(3);//Round
+        sheet2.getRange(id_position,10).setValue(Point);//Point
+        sheet2.getRange(id_position,11).setValue(Time);//Time
+      }
+
       //Best Score 判定
-      if(!sheet2.getRange(id_position,9).getValue()){//2ndが空->1stがBest
-        sheet2.getRange(id_position,3).setValue(sheet2.getRange(id_position,6).getValue());
-        sheet2.getRange(id_position,4).setValue(sheet2.getRange(id_position,7).getValue());
-        sheet2.getRange(id_position,5).setValue(sheet2.getRange(id_position,8).getValue());
-      }
-      else if(sheet2.getRange(id_position,6).getValue()>sheet2.getRange(id_position,9).getValue()){//1st>2nd -> 1stがBest
-        sheet2.getRange(id_position,3).setValue(sheet2.getRange(id_position,6).getValue());
-        sheet2.getRange(id_position,4).setValue(sheet2.getRange(id_position,7).getValue());
-        sheet2.getRange(id_position,5).setValue(sheet2.getRange(id_position,8).getValue());
-      }
-      else if(sheet2.getRange(id_position,6).getValue()<sheet2.getRange(id_position,9).getValue()){//1st<2nd -> 2ndがBest
-        sheet2.getRange(id_position,3).setValue(sheet2.getRange(id_position,9).getValue());
-        sheet2.getRange(id_position,4).setValue(sheet2.getRange(id_position,10).getValue());
-        sheet2.getRange(id_position,5).setValue(sheet2.getRange(id_position,11).getValue());
-      }
-      else if(sheet2.getRange(id_position,6).getValue()==sheet2.getRange(id_position,9).getValue()){//1st=2nd -> Time判定
-        if(sheet2.getRange(id_position,7).getValue()<sheet2.getRange(id_position,10).getValue()){//1st<2nd -> 1stがBest
-          sheet2.getRange(id_position,3).setValue(sheet2.getRange(id_position,6).getValue());
-          sheet2.getRange(id_position,4).setValue(sheet2.getRange(id_position,7).getValue());
-          sheet2.getRange(id_position,5).setValue(sheet2.getRange(id_position,8).getValue());
+      var changed=true;
+      while(1){
+        if(changed){
+          changed=false;
+
+          var round_1st = sheet2.getRange(id_position,3).getValue();
+          var round_2nd = sheet2.getRange(id_position,6).getValue();
+          var round_3rd = sheet2.getRange(id_position,9).getValue();
+          var point_1st = sheet2.getRange(id_position,4).getValue();
+          var point_2nd = sheet2.getRange(id_position,7).getValue();
+          var point_3rd = sheet2.getRange(id_position,10).getValue();
+          var time_1st = sheet2.getRange(id_position,5).getValue();
+          var time_2nd = sheet2.getRange(id_position,8).getValue();
+          var time_3rd = sheet2.getRange(id_position,11).getValue();
+          
+          //点数入れ替え(3位から)
+          if(point_3rd>point_2nd){//3>2
+            if(point_3rd>point_1st){//3>1
+              //3rdを1stに
+              sheet2.getRange(id_position,3).setValue(round_3rd);//Round
+              sheet2.getRange(id_position,4).setValue(point_3rd);//Point
+              sheet2.getRange(id_position,5).setValue(time_3rd);//Time
+              //1stを3rdに
+              sheet2.getRange(id_position,9).setValue(round_1st);//Round
+              sheet2.getRange(id_position,10).setValue(point_1st);//Point
+              sheet2.getRange(id_position,11).setValue(time_1st);//Time
+            }
+            else{//3<1,3==1
+              //3rdを2ndに
+              sheet2.getRange(id_position,6).setValue(round_3rd);//Round
+              sheet2.getRange(id_position,7).setValue(point_3rd);//Point
+              sheet2.getRange(id_position,8).setValue(time_3rd);//Time
+              //2ndを3rdに
+              sheet2.getRange(id_position,9).setValue(round_2nd);//Round
+              sheet2.getRange(id_position,10).setValue(point_2nd);//Point
+              sheet2.getRange(id_position,11).setValue(time_2nd);//Time
+            }
+            changed=true;
+          }
+
+          point_1st = sheet2.getRange(id_position,4).getValue();
+          point_2nd = sheet2.getRange(id_position,7).getValue();
+          
+          //点数入れ替え(2位から)
+          if(point_2nd>point_1st){//2>1
+            round_1st = sheet2.getRange(id_position,3).getValue();
+            round_2nd = sheet2.getRange(id_position,6).getValue();
+            time_1st = sheet2.getRange(id_position,5).getValue();
+            time_2nd = sheet2.getRange(id_position,8).getValue();
+
+            //2ndを1stに
+            sheet2.getRange(id_position,3).setValue(round_2nd);//Round
+            sheet2.getRange(id_position,4).setValue(point_2nd);//Point
+            sheet2.getRange(id_position,5).setValue(time_2nd);//Time
+            //1stを2ndに
+            sheet2.getRange(id_position,6).setValue(round_1st);//Round
+            sheet2.getRange(id_position,7).setValue(point_1st);//Point
+            sheet2.getRange(id_position,8).setValue(time_1st);//Time
+
+            changed=true;
+          }
+
+          point_2nd = sheet2.getRange(id_position,7).getValue();
+          point_3rd = sheet2.getRange(id_position,10).getValue();
+
+          //タイム入れ替え(2位と3位)
+          if(point_2nd==point_3rd){
+            time_2nd = sheet2.getRange(id_position,8).getValue();
+            time_3rd = sheet2.getRange(id_position,11).getValue();
+
+            if(time_2nd>time_3rd){
+              round_2nd = sheet2.getRange(id_position,6).getValue();
+              round_3rd = sheet2.getRange(id_position,9).getValue();
+
+              //3rdを2ndに
+              sheet2.getRange(id_position,6).setValue(round_3rd);//Round
+              sheet2.getRange(id_position,7).setValue(point_3rd);//Point
+              sheet2.getRange(id_position,8).setValue(time_3rd);//Time
+              //2ndを3rdに
+              sheet2.getRange(id_position,9).setValue(round_2nd);//Round
+              sheet2.getRange(id_position,10).setValue(point_2nd);//Point
+              sheet2.getRange(id_position,11).setValue(time_2nd);//Time
+              
+              changed=true;
+            }
+          }
+
+          point_1st = sheet2.getRange(id_position,4).getValue();
+          point_2nd = sheet2.getRange(id_position,7).getValue();
+        
+          //タイム入れ替え(1位と2位)
+          if(point_1st==point_2nd){
+            time_1st = sheet2.getRange(id_position,5).getValue();
+            time_2nd = sheet2.getRange(id_position,8).getValue();
+
+            if(time_1st>time_2nd){
+              round_1st = sheet2.getRange(id_position,3).getValue();
+              round_2nd = sheet2.getRange(id_position,6).getValue();
+
+              //2ndを1stに
+              sheet2.getRange(id_position,3).setValue(round_2nd);//Round
+              sheet2.getRange(id_position,4).setValue(point_2nd);//Point
+              sheet2.getRange(id_position,5).setValue(time_2nd);//Time
+              //1stを2ndに
+              sheet2.getRange(id_position,6).setValue(round_1st);//Round
+              sheet2.getRange(id_position,7).setValue(point_1st);//Point
+              sheet2.getRange(id_position,8).setValue(time_1st);//Time
+
+              changed=true
+            }
+          }
         }
-        else if(sheet2.getRange(id_position,7).getValue()>sheet2.getRange(id_position,10).getValue()){//1st>2nd -> 2ndがBest
-          sheet2.getRange(id_position,3).setValue(sheet2.getRange(id_position,9).getValue());
-          sheet2.getRange(id_position,4).setValue(sheet2.getRange(id_position,10).getValue());
-          sheet2.getRange(id_position,5).setValue(sheet2.getRange(id_position,11).getValue());
-        }
-        else if(sheet2.getRange(id_position,7).getValue()==sheet2.getRange(id_position,10).getValue()){//1st=2nd -> Retire判定
-          if((sheet2.getRange(id_position,8).getValue()=="No")&&(sheet2.getRange(id_position,11).getValue()=="Yes")){//1st
-            sheet2.getRange(id_position,3).setValue(sheet2.getRange(id_position,6).getValue());
-            sheet2.getRange(id_position,4).setValue(sheet2.getRange(id_position,7).getValue());
-            sheet2.getRange(id_position,5).setValue(sheet2.getRange(id_position,8).getValue());
-          }
-          else if((sheet2.getRange(id_position,8).getValue()=="Yes")&&(sheet2.getRange(id_position,11).getValue()=="No")){//2nd
-            sheet2.getRange(id_position,3).setValue(sheet2.getRange(id_position,9).getValue());
-            sheet2.getRange(id_position,4).setValue(sheet2.getRange(id_position,10).getValue());
-            sheet2.getRange(id_position,5).setValue(sheet2.getRange(id_position,11).getValue());
-          }
-          else if(sheet2.getRange(id_position,8).getValue()==sheet2.getRange(id_position,11).getValue()){//1st
-            sheet2.getRange(id_position,3).setValue(sheet2.getRange(id_position,6).getValue());
-            sheet2.getRange(id_position,4).setValue(sheet2.getRange(id_position,7).getValue());
-            sheet2.getRange(id_position,5).setValue(sheet2.getRange(id_position,8).getValue());
-          }
+        else{
+          break;
         }
       }
       //Best Score判定終了
       
-      var sheet3 = getSheet('Ranking');//Sheetの指定
+      var sheet3 = getSheet('Rank');//Sheetの指定
       const columnBVals3 = sheet3.getRange('B:B').getValues(); // A列値を配列で取得
       const LastRow3 = columnBVals3.filter(String).length;  //空白を除き、配列の数を取得
       
       var rank=0;
-      for(var j=2;j<LastRow3+1;j++){
+      for(var j=2;j<=LastRow3;j++){
         if(sheet2.getRange(id_position,1).getValue()==sheet3.getRange(j,2).getValue()){
           rank=j;
           break;
         }
       }
-      sheet3.getRange(rank,4).setValue(sheet2.getRange(id_position,3).getValue());
-      sheet3.getRange(rank,5).setValue(sheet2.getRange(id_position,4).getValue());
-      sheet3.getRange(rank,6).setValue(sheet2.getRange(id_position,5).getValue());
+      sheet3.getRange(rank,4).setValue(sheet2.getRange(id_position,4).getValue());
+      sheet3.getRange(rank,5).setValue(sheet2.getRange(id_position,5).getValue());
       
       sheet.getRange(i,check_column).setValue("Yes");//checkした行に印をつける
     }
   }
 }
+
+/*========================================ここまで========================================*/
 
 function sort(){
   var sheet = getSheet('Ranking');//Sheetの指定
---
+++ 2022/SeniorEx/Main/setting.js
@@ -0,0 +1,142 @@
+//時計関係の配列定義
+var StartTime_array = new Array(0,0,0,0);
+var EndTime_array = new Array(0,0,0,0);
+var RunningTime_array = new Array(0,0,0,0);
+
+//点数計算用の配列定義
+var Score_array = new Array(13).fill(0);
+
+//点数リセット関数
+function Score_reset(){
+    for(let i=0; i<13 ; i++ ){ Score_array[i]=0; }
+}
+
+//点数合計関数
+function Score_sum(){
+    Score_array[0]=0;
+    for(let i=1; i<13 ; i++ ){
+        Score_array[0]+=Score_array[i];
+    }
+}
+
+//点数計算・確認画面生成関数
+function Scoring(mission,selected){
+    let each_point;
+    switch(mission){
+        case 1:
+            each_point = 6;
+            document.getElementById("check_M1-1").innerText = selected;
+            document.getElementById("check_M1-1_Total").innerText = selected*each_point;
+            break;
+        case 2:
+            each_point = 10;
+            document.getElementById("check_M1-2").innerText = selected;
+            document.getElementById("check_M1-2_Total").innerText = selected*each_point;
+            break;
+        case 3:
+            each_point = 16;
+            document.getElementById("check_M1-3").innerText = selected;
+            document.getElementById("check_M1-3_Total").innerText = selected*each_point;
+            break;
+        case 4:
+            each_point=6;
+            document.getElementById("check_M2-1").innerText = selected;
+            document.getElementById("check_M2-1_Total").innerText = selected*each_point;
+            break;
+        case 5:
+            each_point=-6;
+            document.getElementById("check_M2-2").innerText = selected;
+            document.getElementById("check_M2-2_Total").innerText = selected*each_point;
+            break;
+        case 6:
+            each_point=10;
+            document.getElementById("check_M2-3").innerText = selected;
+            document.getElementById("check_M2-3_Total").innerText = selected*each_point;
+            break;
+        case 7:
+            each_point=14;
+            document.getElementById("check_M2-4").innerText = selected;
+            document.getElementById("check_M2-4_Total").innerText = selected*each_point;
+            break;
+        case 8:
+            each_point=13;
+            document.getElementById("check_M3-1").innerText = selected;
+            document.getElementById("check_M3-1_Total").innerText = selected*each_point;
+            break;
+        case 9:
+            each_point=13;
+            document.getElementById("check_M4-1").innerText = selected;
+            document.getElementById("check_M4-1_Total").innerText = selected*each_point;
+            break;
+        case 10:
+            each_point=4;
+            document.getElementById("check_M5-1").innerText = selected;
+            document.getElementById("check_M5-1_Total").innerText = selected*each_point;
+            break;
+        case 11:
+            each_point=2;
+            document.getElementById("check_M5-2").innerText = selected;
+            document.getElementById("check_M5-2_Total").innerText = selected*each_point;
+            break;
+        case 12:
+            each_point=2;
+            document.getElementById("check_M5-3").innerText = selected;
+            document.getElementById("check_M5-3_Total").innerText = selected*each_point;
+            break;
+        default:
+            console.log("ERROR!!");
+            break;
+    }
+    Score_array[mission] = each_point * selected;
+    Score_sum();
+    document.getElementById("Point-text").innerHTML = Score_array[0];
+    document.getElementById("Point-textbox").value = Score_array[0];
+}
+
+//現在時刻表示関数
+function CurrentTime(){
+    let now = new Date();
+    let CureentTime_array = new Array(3);
+    CureentTime_array[0] = now.getHours();
+    CureentTime_array[1] = now.getMinutes();
+    CureentTime_array[2] = now.getSeconds();
+
+    if(CureentTime_array[1]<10){
+        CureentTime_array[1]="0"+CureentTime_array[1];
+    }
+    if(CureentTime_array[2]<10){
+        CureentTime_array[2]="0"+CureentTime_array[2];
+    }
+
+    document.getElementById("CurrentTime-text").innerHTML = CureentTime_array[0]+":"+CureentTime_array[1]+":"+CureentTime_array[2];
+    setTimeout("CurrentTime()", 1000);
+}
+
+//Teamname,Round,Timeの処理
+window.addEventListener('DOMContentLoaded', function(){
+    var input_time = document.getElementById("Time-textbox");
+    input_time.addEventListener("change",function(){
+        document.getElementById("Time-text").innerHTML = input_time.value;
+    });
+
+    var select_teamname = document.getElementById("Teamname_selector");
+    select_teamname.addEventListener("change",function(){
+        var num = select_teamname.selectedIndex;
+        document.getElementById("Teamname-text").innerHTML = select_teamname.options[num].innerText;
+    });
+});
+
+function Round_set(text){
+    document.getElementById("Round-text").innerHTML = text;
+}
+
+//チームリストの設定
+function setTeam(){
+    //プルダウンリストをループ処理で値を取り出してセレクトボックスにセットする
+    for(var i=0;i<list.length;i++){
+    let opt = document.createElement("option");
+    opt.value = list[i].val;  //value値
+    opt.text = list[i].txt;   //テキスト値
+    document.getElementById("Teamname_selector").appendChild(opt);
+    }
+};

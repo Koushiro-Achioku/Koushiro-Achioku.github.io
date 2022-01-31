34f661b3f6cb2d0f4bf1dd921a3909fa107627b6
Koushiro Achioku <achioku.koushiro@gmail.com>
2022-01-31 13:49:22 +0900

    commit by Koshiro Achioku at 2022/1/31 13:49

--- 2022/ElementaryEx/Result/gas/Code.js
+++ 2022/ElementaryEx/Result/gas/Code.js
@@ -12,6 +12,7 @@ function doGet(e) {
     readValue(id,3);
     var htmlOutput = HtmlService.createTemplateFromFile("index").evaluate();
     htmlOutput.addMetaTag('viewport','width=device-width, initial-scale=1, user-scalable=no');
+    htmlOutput.setTitle(id + ' - Result');
     htmlOutput.setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
     return htmlOutput;
 }

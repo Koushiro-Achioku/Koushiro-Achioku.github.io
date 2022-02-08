973013f484fae092824e23fd40892349585ac2cc
Koushiro Achioku <achioku.koushiro@gmail.com>
2022-02-08 09:55:18 +0900

    commit by Koushiro at 2022/2/8 9:55

---
+++ 2022/ElementaryBs/Ranking/gas/Code.js
@@ -0,0 +1,22 @@
+function doGet() {
+    var htmlOutput = HtmlService.createTemplateFromFile("index").evaluate();
+    htmlOutput.addMetaTag('viewport','width=device-width, initial-scale=1, user-scalable=no');
+    htmlOutput.setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+    return htmlOutput;
+}
+
+function getSheet(name){
+    // SSIDからスプレッドシートの取得
+    //https://docs.google.com/spreadsheets/d/1qhXEUuBwR-iccgf4TLTa8cmEY4LvFpSOKv5CX0NHV-0/edit#gid=0
+    var ssid = '1qhXEUuBwR-iccgf4TLTa8cmEY4LvFpSOKv5CX0NHV-0';
+    var ss = SpreadsheetApp.openById(ssid);
+    // 指定されたシート名からシートを取得して返却
+    var sheet = ss.getSheetByName(name);
+    return sheet;
+}
+
+function getData(name) {
+    // 指定したシートからデータを取得
+    var values = getSheet(name).getDataRange().getValues();
+    return values;
+}
---
+++ 2022/ElementaryBs/Result/gas/Code.js
@@ -0,0 +1,137 @@
+const column = 7;
+var id=0;
+
+var ss_value_r1 = new Array(column);
+var ss_value_r2 = new Array(column);
+var ss_value_r3 = new Array(column);
+
+function doGet(e) {
+    id = e.parameter.id;
+    readValue(id,1);
+    readValue(id,2);
+    readValue(id,3);
+    var htmlOutput = HtmlService.createTemplateFromFile("index").evaluate();
+    htmlOutput.addMetaTag('viewport','width=device-width, initial-scale=1, user-scalable=no');
+    htmlOutput.setTitle(id + ' - Result');
+    htmlOutput.setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+    return htmlOutput;
+}
+
+function getSheet(name){
+    // SSIDからスプレッドシートの取得
+    //https://docs.google.com/spreadsheets/d/1qhXEUuBwR-iccgf4TLTa8cmEY4LvFpSOKv5CX0NHV-0/edit#gid=0
+    var ssid = '1qhXEUuBwR-iccgf4TLTa8cmEY4LvFpSOKv5CX0NHV-0';
+    var ss = SpreadsheetApp.openById(ssid);
+    // 指定されたシート名からシートを取得して返却
+    var sheet = ss.getSheetByName(name);
+    return sheet;
+}
+
+function getData(name) {
+    // 指定したシートからデータを取得
+    var values = getSheet(name).getDataRange().getValues();
+    return values;
+}
+
+function writeValue(id_read,round,mission){
+    var id_row=0;
+    var base_row=0;
+
+    var sheet = getSheet('Base');//Sheetの指定
+
+    var sheet2 = getSheet('Total');//Sheetの指定
+    const columnBVals2 = sheet2.getRange('A:A').getValues(); // A列値を配列で取得
+    const LastRow2 = columnBVals2.filter(String).length;  //空白を除き、配列の数を取得
+
+    for(var i=3; i<=LastRow2; i++){
+        if(id_read==sheet2.getRange(i,1).getValue())
+            id_row=i;
+    }
+
+    if(sheet2.getRange(id_row,11+round*3).getValue()){
+        base_row=sheet2.getRange(id_row,11+round*3).getValue();
+        return sheet.getRange(base_row,5+mission).getValue();
+    }
+    else{
+        return "";
+    }
+}
+
+function writeID(id_read){
+    var id_row=0;
+    var sheet2 = getSheet('Total');//Sheetの指定
+    const columnBVals2 = sheet2.getRange('A:A').getValues(); // A列値を配列で取得
+    const LastRow2 = columnBVals2.filter(String).length;  //空白を除き、配列の数を取得
+    
+    for(var i=3; i<=LastRow2+1; i++){
+        if(id_read==sheet2.getRange(i,1).getValue())
+            id_row=i;
+    }
+
+    return sheet2.getRange(id_row,2).getValue();
+}
+
+function readValue(id_read,round){
+    var id_row=0;
+    var base_row=0;
+
+    var sheet = getSheet('Base');//Sheetの指定
+
+    var sheet2 = getSheet('Total');//Sheetの指定
+    const columnBVals2 = sheet2.getRange('A:A').getValues(); // A列値を配列で取得
+    const LastRow2 = columnBVals2.filter(String).length;  //空白を除き、配列の数を取得
+
+    for(var i=3; i<=LastRow2+1; i++){
+        if(id_read==sheet2.getRange(i,1).getValue())
+            id_row=i;
+    }
+
+    if(sheet2.getRange(id_row,11+round*3).getValue()){
+        base_row=sheet2.getRange(id_row,11+round*3).getValue();
+
+        for(var ii=6;ii<=17;ii++){
+            switch(round){
+                case 1:
+                    ss_value_r1[ii-5] = sheet.getRange(base_row,ii).getValue();
+                    break;
+                case 2:
+                    ss_value_r2[ii-5] = sheet.getRange(base_row,ii).getValue();
+                    break;
+                case 3:
+                    ss_value_r3[ii-5] = sheet.getRange(base_row,ii).getValue();
+                    break;
+            }
+        }
+
+        switch(round){
+            case 1:
+                ss_value_r1[0] = sheet.getRange(base_row,4).getValue();//合計点
+                ss_value_r1[column-1] = sheet.getRange(base_row,5).getValue();//タイム
+                break;
+            case 2:
+                ss_value_r2[0] = sheet.getRange(base_row,4).getValue();//合計点
+                ss_value_r2[column-1] = sheet.getRange(base_row,5).getValue();//タイム
+                break;
+            case 3:
+                ss_value_r3[0] = sheet.getRange(base_row,4).getValue();//合計点
+                ss_value_r3[column-1] = sheet.getRange(base_row,5).getValue();//タイム
+                break;
+        }
+        
+    }
+    else{
+        for(var ii=0;ii<column;ii++){
+            switch(round){
+                case 1:
+                    ss_value_r1[ii] = "";
+                    break;
+                case 2:
+                    ss_value_r2[ii] = "";
+                    break;
+                case 3:
+                    ss_value_r3[ii] = "";
+                    break;
+            }
+        }
+    }
+}
